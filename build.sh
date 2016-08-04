3a332f3d769f2f0666649c9b27b5445eeca433a6
rbn42 <bl100@students.waikato.ac.nz>
2016-08-04 15:52:01 +1200

    build.sh

--- build.sh
+++ build.sh
@@ -4,12 +4,19 @@ export ORGFILE=./report.org
 #pdf暂且弄不了中文,所以还是输出html吧
 rm ./report.pdf
 pandoc \
-    -o ./report.pdf \
+    -s -o ./report.pdf \
     --latex-engine=xelatex \
     $ORGFILE
 
     #--template=./pm-template.latex \
     #-V mainfont=Hei \
+
+rm ./report.tex
+pandoc \
+    -s -o ./report.tex \
+    --latex-engine=xelatex \
+    $ORGFILE
+
 
 rm ./report.html
 pandoc -s -o ./report.html $ORGFILE
