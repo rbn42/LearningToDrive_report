7b07a80b8c2623914d9c7037cf3fe79739112ba1
rbn42 <bl100@students.waikato.ac.nz>
2016-08-31 08:27:38 +1200

    code/nn_2d.py

--- code/nn_2d.py
+++ code/nn_2d.py
@@ -1,13 +1,13 @@
-input_layer = DataLayer()
+input_layer = DataLayeaar()
 # normalize input data
-input_layer = Layer(input_layer, fun=lambda x: (x - T.mean(x)) / T.std(x))
+input_layer = Layer(input_layer, filter=lambda x: (x - T.mean(x)) / T.std(x))
 
 layer1 = DenseLayer(input_layer=input_layer, n_in=84,
                     n_out=32, std=.005, bias=0)
-layer1 = Sigmoid(layer1)
+layer1 = ReLU(layer1)
 
 layer2 = DenseLayer(input_layer=layer1, n_in=32, n_out=32, std=.005, bias=0)
-layer2 = Sigmoid(layer2)
+layer2 = ReLU(layer2)
 
 output_layer = DenseLayer(input_layer=layer2, n_in=32,
                           n_out=n_actions, std=.005, bias=0)
