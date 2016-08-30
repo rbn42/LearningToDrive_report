428841e1f56dd74e52e47e3624a4fc752544bd01
rbn42 <bl100@students.waikato.ac.nz>
2016-08-31 08:13:25 +1200

    images/nn_2d.digraph

--- dot.py
+++ dot.py
@@ -1,5 +1,13 @@
 """
 yaourt graphviz
+
+另一个办法
+http://orgmode.org/worg/org-contrib/babel/languages/ob-doc-dot.html
+emacs中可以执行dot,并且似乎可以通过table数据动态生成,图片和table因此就互相匹配了
+
+这种办法的问题是,所有代码都inline的方式嵌入在了org文件中,
+让人感觉很混乱.
+matplotlib什么的还是放在独立脚本中,手动生成图片后再引入org比较好.
 """
 import sys
 import os.path
